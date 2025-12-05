1582da99cd817fe0f8884f38662561568052bb12
Eric Kunzel <kunzel.eric@gmail.com>
2025-12-05 17:19:52 -0300

    created test for `/api/v1/status` route

--- src/tests/api/v1/status.test.ts
+++ src/tests/api/v1/status.test.ts
@@ -1,9 +1,21 @@
-import { expect, test } from "vitest"
+import { beforeAll, describe, expect, it } from "vitest"
 
-test("should return status code 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status", {
-    method: "GET",
+let response: Response
+
+describe("API v1 STATUS ROUTE TEST", () => {
+  beforeAll(async () => {
+    response = await fetch("http://localhost:3000/api/v1/status", {
+      method: "GET",
+    })
   })
 
-  expect(response.status).toBe(200)
+  it("should return status code 200", async () => {
+    expect(response.status).toBe(200)
+  })
+
+  it("should returned database as a dependency", async () => {
+    const body = await response.json()
+
+    expect(body.dependencies).toHaveProperty("database")
+  })
 })
